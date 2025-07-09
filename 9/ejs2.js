0748cf9360cecbd37634b1fe9423a4e98ee95cc1
kimjiwon0706221 <a0105171713168@gmail.com>
2025-07-09 16:16:27 +0900

    1

---
+++ 9/ejs2.js
@@ -0,0 +1,2 @@
+document.write("ehs2.js");
+document.write("<div style='color: blue; font-size: 20px;'>외부 자바스크립트 파일</div>");
---
+++ 9/njs2.js
@@ -0,0 +1,3 @@
+document.write("njs2.js는 njs1.js에 포함");
+document.write("div style='color: blue; font-size: 20px;'>어ㅣ부 자바스크립트 파일</div>");
+document.write("<script src='./njs3.js></script>");
